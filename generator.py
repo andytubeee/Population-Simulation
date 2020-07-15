23f95f768057b7abdc9608a7b8fb131e022da05e
andytubeee <andy.tubeee@gmail.com>
2020-07-15 00:59:10 -0400

    first commit

---
+++ generator.py
@@ -0,0 +1,91 @@
+import csv
+import random
+import datetime
+import time
+import numpy as np
+import pandas as pd
+import scipy.stats as stats
+from scipy.stats import randint
+from scipy.stats import truncnorm
+import math
+import array
+import datetime
+import time
+
+
+def str_time_prop(start, end, format, prop):
+    stime = time.mktime(time.strptime(start, format))
+    etime = time.mktime(time.strptime(end, format))
+
+    ptime = stime + prop * (etime - stime)
+
+    return time.strftime(format, time.localtime(ptime))
+
+
+def random_date(start, end, prop):
+    return str_time_prop(start, end, '%m/%d/%Y %I:%M %p', prop)
+
+
+gender_choice = [1, 2]
+gender_dist = [.48, .52]
+genders = []
+
+
+def generate(amount):
+    for i in range(0, amount):
+        genders.append(str(random.choices(gender_choice, gender_dist)
+                           ).replace('[', '').replace(']', ''))
+
+
+amount_generate = 100
+
+generate(amount_generate)
+
+ten_percent_age = int((amount_generate/100)*10)
+
+young_age = []
+old_age = []
+age = []
+
+for i in range(0, ten_percent_age):
+    young_age.append(np.random.normal(loc=35))
+
+for i in range(0, amount_generate - ten_percent_age):
+    old_age.append(np.random.normal(loc=85))
+
+age = young_age + old_age
+
+
+# Birth Date
+
+birth_date = []
+birth_year = []
+birth_month = []
+birth_day = []
+yearNow = int(str(datetime.datetime.now()).split()[0].split('-')[0])
+
+for Age in age:
+    birth_year.append(yearNow - round(Age))
+    birth_month.append(round(float("0."+str(Age).split('.')[1])*12)) if round(
+        float("0."+str(Age).split('.')[1])*12) != 0 else birth_month.append(1)
+
+    if (round(float("0."+str(Age).split('.')[1])*12) == 2):
+        birth_day.append(random.randint(0, 28))
+    elif((round(float("0."+str(Age).split('.')[1])*12) == 2) and int(birth_year) % 4 == 0):
+        birth_day.append(random.randint(0, 29))
+    else:
+        birth_day.append(random.randint(0, 30))
+
+
+# with open('output.csv', 'w', newline='') as csvfile:
+#     fieldnames = ["Gender", "Age"]
+#     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+#     writer.writeheader()
+#     for Gender in genders:
+
+#         writer.writerow(
+#             {'Gender': Gender})
+
+df = pd.DataFrame({"Genders": genders, "Age": age,
+                   "Birth Year": birth_year, "Birth Month": birth_month, "Birth Day": birth_day})
+df.to_csv("output.csv", index=False)
